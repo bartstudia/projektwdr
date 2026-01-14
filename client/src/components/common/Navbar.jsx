23546ac7696b5a845380e1fb31796fba8206bdc1
Kabak <kacperkabak456@gmail.com>
2026-01-14 19:04:12 +0100

    Revert "docs"
    This reverts commit 3e961364a3403c5d3c7eacfe196dbf1ed0e293ec.

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -15,7 +15,6 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          <img src="/logo.png" alt="Logo" className="navbar-logo-image" />
           Rezerwacje Wędkarskie
         </Link>
 
